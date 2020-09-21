92e2473006bca546617c22aa1b16b4dc66ea5ef2
Madongming <elsove812@gmail.com>
2020-09-21 13:48:56 +0800

    Add max number of element.

--- contract/contracts/SimpleStorage.sol
+++ contract/contracts/SimpleStorage.sol
@@ -7,13 +7,24 @@ contract ParseJson {
     JsmnSolLib.Token[] tokens;
     uint tokenCount;
 
+    uint maxNumberElement;
 
+    constructor() public {
+        maxNumberElement = 100;
+    }
+    
     function parse(string memory json, uint numberElements) public {
+        require(numberElements <= maxNumberElement,
+                "numberElements can't biger than max number of element.");
         JsmnSolLib.Token[] memory tokens_;
         (success, tokens_, tokenCount) = JsmnSolLib.parse(json, numberElements);
         for (uint i = 0; i < tokens_.length; i++) {
             tokens.push(tokens_[i]);
         }
+    }
+
+    function setMaxNumElements(uint maxNumber) public {
+        maxNumberElement = maxNumber;
     }
 }
                 
